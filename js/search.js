3947e735cfebe13f4e73edf051e91328fa0b9933
marco sica <marcosica99@gmail.com>
2021-06-19 17:44:09 +0200

    next fix for js search

--- js/search.js
+++ js/search.js
@@ -1,3 +1,6 @@
 function validate() {
-    $("#testo").html("Fazzo");
+    document.getElementById("toHide").style.visibility = "hidden";
+    document.getElementById("testo").innerHTML="Ecco le richieste nella zona di Salerno";
+    document.getElementById("testo").className="alert alert-secondary";
+
 }
